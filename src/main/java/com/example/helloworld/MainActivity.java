d2ae4fef9dcd46c361a6c24f2b0dd7ae97319537
voquangduc2205 <henryduca2k45pbc@gmail.com>
2022-04-10 15:50:55 +0700

    codelab 1.1

---
+++ src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Main Activity", "Hello World");
+    }
+}
